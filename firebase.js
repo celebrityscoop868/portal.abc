8f2e16791825a4d1a9c1f9dd4df4aba7eccae8e5
celebrityscoop868 <celebrityscoop868@gmail.com>
2026-02-08 11:53:50 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyA6kZ4LL22vPr5XeTCdtcnCqfs_2g_jjqw",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "sunpower-portal.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "sunpower-portal",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "sunpower-portal.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "557829218180",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:557829218180:web:f5ae1a4362e88a271e87d1",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-70553ET048"
+  apiKey: "AIzaSyA6kZ4LL22vPr5XeTCdtcnCqfs_2g_jjqw",
+  authDomain: "sunpower-portal.firebaseapp.com",
+  projectId: "sunpower-portal",
+  storageBucket: "sunpower-portal.firebasestorage.app",
+  messagingSenderId: "557829218180",
+  appId: "1:557829218180:web:f5ae1a4362e88a271e87d1",
+  measurementId: "G-70553ET048"
 };
 
 export const app = initializeApp(firebaseConfig);
